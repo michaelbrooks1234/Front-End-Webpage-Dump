b8551e0b9f58a3dbb03cd74f35f16ce6ea4bf8bd
Michael Brooks <66489250+michaelbrooks1234@users.noreply.github.com>
2021-08-21 16:47:28 -0400

    Update Index.js

--- Portfolio/Index.js
+++ Portfolio/Index.js
@@ -1,7 +1,15 @@
-function widthWatcher(width){
-    return null;
-}
+let portfolio1 = document.getElementById("portfolio1");
+let nametitle = document.getElementById("nametitle");
+let container = document.getElementById("container");
 
-
-
-document.getElementById("john").addEventListener()
+window.onscroll = (event) => {
+    if(window.scrollY > 0){
+        portfolio1.style.opacity = 1-window.scrollY/500;
+        nametitle.style.opacity = 1-window.scrollY/300;
+    }if(window.scrollY > 100){
+        container.style.transform = "translateY(0)";
+        container.style.animation = "fadein linear 1s";
+    }else{
+        container.style.transform = "translateY(-100%)";
+    }
+};
