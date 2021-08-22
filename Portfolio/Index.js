a606684343df0bb52e0e85cc65c770f0b317a6f1
Michael Brooks <66489250+michaelbrooks1234@users.noreply.github.com>
2021-08-22 12:56:13 -0400

    Update Index.js

--- Portfolio/Index.js
+++ Portfolio/Index.js
@@ -1,15 +1,31 @@
 let portfolio1 = document.getElementById("portfolio1");
 let nametitle = document.getElementById("nametitle");
 let container = document.getElementById("container");
+let about = document.getElementById("about");
+let projects = document.getElementById("projects");
+let skills = document.getElementById("skills");
+
+let bruh = window.innerHeight/100;
 
 window.onscroll = (event) => {
     if(window.scrollY > 0){
         portfolio1.style.opacity = 1-window.scrollY/500;
         nametitle.style.opacity = 1-window.scrollY/300;
-    }if(window.scrollY > 100){
-        container.style.transform = "translateY(0)";
-        container.style.animation = "fadein linear 1s";
-    }else{
-        container.style.transform = "translateY(-100%)";
+        container.style.opacity = 0+window.scrollY/500;
+        if(window.scrollY >= bruh*100 && window.scrollY <= bruh*180){
+            about.style.backgroundColor = "orange";
+            bruh = window.innerHeight/100;
+        }else{
+            about.style.backgroundColor = "";
+        }
+        
+        
+        if(window.scrollY >= bruh*180){
+            projects.style.backgroundColor = "orange";
+            bruh = window.innerHeight/100;
+        }else{
+            projects.style.backgroundColor = "";
+            bruh = window.innerHeight/100;
+        }
     }
 };
